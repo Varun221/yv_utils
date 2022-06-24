ccdaba05afc39b3d0f3174f0273aff575fc0f489
Varun221 <yerram.varun@gmail.com>
2022-06-24 23:50:39 +0530

    ignored pycache

--- setup.py
+++ setup.py
@@ -3,11 +3,11 @@ from setuptools import setup, find_packages
 
 setup(
     name='yv_utils',
-    version='0.5',
+    version='0.6',
     license='MIT',
     author="Yerram Varun",
     author_email='yerram.varun@gmail.com',
-    packages=find_packages('src', exclude=['.git']),
+    packages=find_packages('src', exclude=['.git', '.gitignore']),
     package_dir={'': 'src'},
     url='https://github.com/Varun221/yv_utils',
     keywords='utils package',
